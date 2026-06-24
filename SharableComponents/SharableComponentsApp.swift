7fc7bc9901b9d0058edcca8cca2c1cd8208571aa
Don-Di-Eggo <30983384+Don-Di-Eggo@users.noreply.github.com>
2026-06-24 12:34:16 -0400

    Initial Commit

---
+++ SharableComponents/SharableComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharableComponentsApp.swift
+//  SharableComponents
+//
+//  Created by James A Kettner on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SharableComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SharableComponentsTests/SharableComponentsTests.swift
@@ -0,0 +1,19 @@
+//
+//  SharableComponentsTests.swift
+//  SharableComponentsTests
+//
+//  Created by James A Kettner on 6/24/26.
+//
+
+import Testing
+@testable import SharableComponents
+
+struct SharableComponentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
